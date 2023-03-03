2539a8a8e9f905747a7f3a77972d3eac9e1a2830
plasroom46 <amos6572@gmail.com>
2023-03-03 10:53:12 +0800

    Merging 2 Packages

---
+++ PrampAlgorithm/Merging 2 Packages/ReadMe.md
@@ -0,0 +1,11 @@
+# Merging 2 Packages
+
+## Hash Table
+
+Time Complexity: $O(n)$
+
+Space Complexity: $O(n)$
+
+## Similar case in leetcode
+
+[1. Two Sum](https://leetcode.com/problems/two-sum/)
---
+++ PrampAlgorithm/Merging 2 Packages/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrampAlgorithm.Merging_2_Packages
+{
+    public class Solution
+    {
+        public int[] GetIndicesOfItemWeights(int[] arr, int limit)
+        {
+            // {weight : index}
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int remainWeight = limit - arr[i];
+                if (map.ContainsKey(remainWeight))
+                    return new int[] { i, map[remainWeight] };
+                map[arr[i]] = i;
+            }
+            return new int[0];
+        }
+
+        public void Run()
+        {
+            var ans = GetIndicesOfItemWeights(new int[] { 4, 6, 10, 15, 16 }, 21);
+            Console.WriteLine(string.Join(",", ans));
+        }
+    }
+}
