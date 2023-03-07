2a1739449fb7b868149545826fd649e0f8bfa962
plasroom46 <amos6572@gmail.com>
2023-03-07 11:19:03 +0800

    Matrix Spiral Copy

---
+++ PrampAlgorithm/Matrix Spiral Copy/ReadMe.md
@@ -0,0 +1,11 @@
+# Matrix Spiral Copy
+
+## Simulation
+
+Time Complexity: $O(row * col)$
+
+Space Complexity: $O(row * col)$ (consider the output space)
+
+## Similar case in leetcode
+
+[54. Spiral Matrix](https://leetcode.com/problems/spiral-matrix/)
---
+++ PrampAlgorithm/Matrix Spiral Copy/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrampAlgorithm.Matrix_Spiral_Copy
+{
+    public class Solution
+    {
+        public int[] SpiralCopy(int[,] inputMatrix)
+        {
+            int row = inputMatrix.GetLength(0);
+            int col = inputMatrix.GetLength(1);
+            int rowLow = 0;
+            int rowHigh = row - 1;
+            int colLow = 0;
+            int colHigh = col - 1;
+            int[] ans = new int[row * col];
+            int index = 0;
+            while (index < row * col)
+            {
+                for (int c = colLow; c <= colHigh && index < row * col; c++)
+                    ans[index++] = inputMatrix[rowLow, c]; // 1 ~ 5
+                rowLow++;
+                for (int r = rowLow; r <= rowHigh && index < row * col; r++)
+                    ans[index++] = inputMatrix[r, colHigh]; // 10, 15, 20
+                colHigh--;
+                for (int c = colHigh; c >= colLow && index < row * col; c--)
+                    ans[index++] = inputMatrix[rowHigh, c]; // 19 ~ 16
+                rowHigh--;
+                for (int r = rowHigh; r >= rowLow && index < row * col; r--)
+                    ans[index++] = inputMatrix[r, colLow]; // 11, 6
+                colLow++;
+            }
+            return ans;
+        }
+
+        public void Run()
+        {
+            int[,] input = new int[,]
+            {
+                {1,    2,   3,  4,    5},
+                {6,    7,   8,  9,   10},
+                {11,  12,  13,  14,  15},
+                {16,  17,  18,  19,  20}
+            };
+            var output = SpiralCopy(input);
+            Console.WriteLine(string.Join(", ", output));
+        }
+    }
+}
