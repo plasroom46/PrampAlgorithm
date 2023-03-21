74ceb0fbb7202413a2402c96def6aaa0a38f72b5
plasroom46 <amos6572@gmail.com>
2023-03-21 10:59:32 +0800

    Toeplitz Matrix

---
+++ PrampAlgorithm/Toeplitz Matrix/ReadMe.md
@@ -0,0 +1,11 @@
+# Toeplitz Matrix
+
+## Brute Force
+
+Time Complexity: $O(m * n)$
+
+Space Complexity: $O(1)$
+
+## Similar case in leetcode
+
+766. Toeplitz Matrix](https://leetcode.com/problems/toeplitz-matrix/description/)
---
+++ PrampAlgorithm/Toeplitz Matrix/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrampAlgorithm.Toeplitz_Matrix
+{
+    public class Solution
+    {
+        public bool IsToeplitz(int[,] arr)
+        {
+            if (arr == null && arr.GetLength(0) == 0)
+                return false;
+            for (int r = 1; r < arr.GetLength(0); r++)
+                for (int c = 1; c < arr.GetLength(1); c++)
+                    if (arr[r, c] != arr[r - 1, c - 1])
+                        return false;
+            return true;
+        }
+
+        public void Run()
+        {
+            //// true
+            //int[,] input = new int[,]
+            //{
+            //    {1,2,3,4},
+            //    {5,1,2,3},
+            //    {6,5,1,2}
+            //};
+
+            // false
+            int[,] input = new int[,]
+            {
+                {1,2,3,4},
+                {5,1,9,3},
+                {6,5,1,2}
+            };
+            Console.WriteLine(IsToeplitz(input));
+        }
+    }
+}
