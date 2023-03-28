a3c22c66ab236c77fec7659e902f8f648fce4155
plasroom46 <amos6572@gmail.com>
2023-03-28 09:44:13 +0800

    Absolute Value Sort

---
+++ PrampAlgorithm/Absolute Value Sort/ReadMe.md
@@ -0,0 +1,9 @@
+# Absolute Value Sort
+
+## Sort
+
+Time Complexity: $O(nlogn)$
+
+Space Complexity: $O(n)$
+
+## Similar case in leetcode
---
+++ PrampAlgorithm/Absolute Value Sort/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrampAlgorithm.Absolute_Value_Sort
+{
+    public class Solution
+    {
+        public int[] AbsoluteValueSort(int[] arr)
+        {
+            Array.Sort(arr, (a, b) => {
+                if (Math.Abs(a) == Math.Abs(b))
+                    return a.CompareTo(b);
+                else return Math.Abs(a).CompareTo(Math.Abs(b));
+            });
+
+            return arr;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(string.Join(", ", AbsoluteValueSort(new int[] { 2, -7, -2, -2, 0 })));
+        }
+    }
+}
