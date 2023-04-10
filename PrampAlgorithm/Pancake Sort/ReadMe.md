45542ba81d0549e0595618535b9bba4e189740ce
plasroom46 <amos6572@gmail.com>
2023-04-10 10:23:53 +0800

    Pancake Sort

---
+++ PrampAlgorithm/Pancake Sort/ReadMe.md
@@ -0,0 +1,11 @@
+# Pairs with Specific Difference
+
+## Simulation
+
+Time Complexity: $O(n^2)$
+
+Space Complexity: $O(1)$
+
+## Similar case in leetcode
+
+[969. Pancake Sorting](https://leetcode.com/problems/pancake-sorting/description/)
---
+++ PrampAlgorithm/Pancake Sort/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrampAlgorithm.Pancake_Sort
+{
+    public class Solution
+    {
+        public int[] PancakeSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                // find the larget number in the range
+                int largeIndex = i;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (arr[largeIndex] < arr[j])
+                        largeIndex = j;
+                }
+                Flip(arr, largeIndex + 1);
+                Flip(arr, i + 1);
+            }
+            return arr;
+        }
+
+        private void Flip(int[] arr, int k)
+        {
+            int l = 0;
+            int r = k - 1;
+            while (l < r)
+            {
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+                l++;
+                r--;
+            }
+            return;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(string.Join(",", PancakeSort(new int[] { 1, 5, 4, 3, 2 })));
+        }
+    }
+}
