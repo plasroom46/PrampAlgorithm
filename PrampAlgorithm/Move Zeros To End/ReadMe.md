bfe46e9f25644be145b096bb20ca14bfaaa31b90
plasroom46 <amos6572@gmail.com>
2023-04-14 11:42:39 +0800

    Move Zeros To End

---
+++ PrampAlgorithm/Move Zeros To End/ReadMe.md
@@ -0,0 +1,10 @@
+﻿# Move Zeros To End
+
+## Two Pointers
+
+Time Complexity: $O(n)$
+
+Space Complexity: $O(1)$
+
+## Similar case in leetcode
+[283. Move Zeroes](https://leetcode.com/problems/move-zeroes/)
---
+++ PrampAlgorithm/Move Zeros To End/Solution_V1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrampAlgorithm.Move_Zeros_To_End
+{
+    public class Solution_V1
+    {
+        public int[] MoveZerosToEnd(int[] arr)
+        {
+            int n = arr.Length;
+            int start = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    arr[start] = arr[i];
+                    start++;
+                }
+            }
+
+            for (int i = start; i < n; i++)
+                arr[i] = 0;
+            return arr;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(string.Join(" ,", new int[] { 1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0 }));
+        }
+    }
+}
