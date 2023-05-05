d615d51f20f49454622d0dd36aaf85a3f5edace0
plasroom46 <amos6572@gmail.com>
2023-05-05 09:58:06 +0800

    Flatten a Dictionary

---
+++ PrampAlgorithm/Flatten a Dictionary/ReadMe.md
@@ -0,0 +1,12 @@
+# Flatten a Dictionary
+
+## Recursion
+
+n: The number of keys in the input dictionary
+
+Time Complexity: $O(n)$
+
+Space Complexity: $O(n)$
+
+## Similar case in leetcode
+[341. Flatten Nested List Iterator](https://leetcode.com/problems/flatten-nested-list-iterator/)
---
+++ PrampAlgorithm/Flatten a Dictionary/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrampAlgorithm.Flatten_a_Dictionary
+{
+    public class Solution
+    {
+        public Dictionary<string, string> FlattenDictionary(Dictionary<string, object> dict)
+        {
+            Dictionary<string, string> ans = new Dictionary<string, string>();
+            List<string> keys = new List<string>();
+            FlattenDictionary(dict, keys, ans);
+            return ans;
+        }
+
+        private void FlattenDictionary(Dictionary<string, object> dict, List<string> keys, Dictionary<string, string> ans)
+        {
+            foreach (var pair in dict)
+            {
+                if (pair.Key != "")
+                    keys.Add(pair.Key);
+                if (pair.Value.GetType() == typeof(string))
+                    ans[string.Join(".", keys)] = (string)pair.Value;
+                else
+                    FlattenDictionary((Dictionary<string, object>)dict[pair.Key], keys, ans);
+                if (pair.Key != "")
+                    keys.RemoveAt(keys.Count - 1);
+            }
+        }
+
+        public void Run()
+        {
+
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            dict["Key1"] = "1";
+            dict["Key2"] = new Dictionary<string, object>();
+            ((Dictionary<string, object>)dict["Key2"])["a"] = "2";
+            ((Dictionary<string, object>)dict["Key2"])["b"] = "3";
+            ((Dictionary<string, object>)dict["Key2"])["c"] = new Dictionary<string, object>();
+            ((Dictionary<string, object>)((Dictionary<string, object>)dict["Key2"])["c"])["d"] = "3";
+            ((Dictionary<string, object>)((Dictionary<string, object>)dict["Key2"])["c"])["e"] = new Dictionary<string, object>();
+            ((Dictionary<string, object>)((Dictionary<string, object>)((Dictionary<string, object>)dict["Key2"])["c"])["e"])[""] = "1";
+
+            var ans = FlattenDictionary(dict);
+            foreach (var pair in ans)
+                Console.WriteLine($"{pair.Key}:{pair.Value}");
+        }
+    }
+}
